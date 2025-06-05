4485dd6218453f5bcb141fff5d20e4f3d3a37054
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-06-06 00:10:15 +0300

    Add login_users

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from app import schemas, models, auth
 from sqlalchemy.orm import Session
-
+from fastapi.security import OAuth2PasswordRequestForm
 from ..dependencies import get_db
 
 router = APIRouter(
@@ -26,3 +26,15 @@ async def register_users(user_data: schemas.UserCreate, db: Session = Depends(ge
     db.commit()
     db.refresh(new_user)
     return new_user
+
+
+@router.post("/login", response_model=schemas.Token)
+async def login_users(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == form_data.username).first()
+    if not user or not auth.verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code=401,
+            detail="Incorrect email or password"
+        )
+    access_token = auth.create_access_token(data={"sub": user.email})
+    return {"access_token": access_token, "token_type": "bearer"}
