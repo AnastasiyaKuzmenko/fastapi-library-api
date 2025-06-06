b0ae3af0999b945f447f72f00c1ba8352a94c39d
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-06-06 13:13:17 +0300

    add update_book()

--- app/routers/books.py
+++ app/routers/books.py
@@ -1,12 +1,47 @@
 from fastapi import APIRouter, Depends, HTTPException
 from ..dependencies import get_db, get_current_user
 from sqlalchemy.orm import Session
-from app import models
+from app import models, schemas
 
 router = APIRouter(
     prefix="/books",
     tags=["Books"]
 )
+
+
+
+
+
+@router.put("/{book_id}", response_model=schemas.BookOut)
+def update_book(
+    book_id: int,
+    book_data: schemas.BookUpdate, 
+    db: Session = Depends(get_db), 
+    current_user: models.User = Depends(get_current_user)
+):
+    book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not book:
+            raise HTTPException(
+            status_code=404,
+            detail="Book not found"
+        )
+    if book_data.title is not None:
+        book.title = book_data.title
+    if book_data.author is not None:
+        book.author = book_data.author
+    if book_data.publication_year is not None:
+        book.publication_year = book_data.publication_year
+    if book_data.isbn is not None:
+        book.isbn = book_data.isbn
+    if book_data.copies_available is not None:
+        book.copies_available = book_data.copies_available
+    if book_data.description is not None:
+        book.description = book_data.description
+
+    db.commit()
+    db.refresh(book)
+    return book
+
 
 @router.delete("/{book_id}")
 def delete_book(
