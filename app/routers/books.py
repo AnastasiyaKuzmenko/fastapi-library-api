4171c08469af6713b52732608e7bb4e151b0087d
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-06-06 14:37:21 +0300

    add get_book() and get_books()

--- app/routers/books.py
+++ app/routers/books.py
@@ -8,20 +8,39 @@ router = APIRouter(
     tags=["Books"]
 )
 
+
+# Get all books
+@router.post("/", response_model=list[schemas.BookOut])
+def get_books(db: Session = Depends(get_db)):
+    return db.query(models.Book).all()
+
+
+# Get book by id
+@router.post("/{book_id}", response_model=schemas.BookOut)
+def get_book(book_id: int, db: Session = Depends(get_db)):
+    book = db.query(models.Book).filter(models.Book.id == book_id).first()
+    if not book:
+        raise HTTPException(
+            status_code=404,
+            detail="Book not found"
+        )
+    return book
+
+
 @router.post("/", response_model=schemas.BookOut)
 def create_book(
-    book_data: schemas.BookCreate, 
-    db: Session = Depends(get_db), 
+    book_data: schemas.BookCreate,
+    db: Session = Depends(get_db),
     current_user: models.User = Depends(get_current_user)
 ):
 
     book = models.Book(
         title=book_data.title,
-        author = book_data.author,
-        publication_year = book_data.publication_year,
-        isbn = book_data.isbn,
-        copies_available = book_data.copies_available,
-        description = book_data.description
+        author=book_data.author,
+        publication_year=book_data.publication_year,
+        isbn=book_data.isbn,
+        copies_available=book_data.copies_available,
+        description=book_data.description
     )
 
     db.add(book)
@@ -30,17 +49,16 @@ def create_book(
     return book
 
 
-
 @router.put("/{book_id}", response_model=schemas.BookOut)
 def update_book(
     book_id: int,
-    book_data: schemas.BookUpdate, 
-    db: Session = Depends(get_db), 
+    book_data: schemas.BookUpdate,
+    db: Session = Depends(get_db),
     current_user: models.User = Depends(get_current_user)
 ):
     book = db.query(models.Book).filter(models.Book.id == book_id).first()
     if not book:
-            raise HTTPException(
+        raise HTTPException(
             status_code=404,
             detail="Book not found"
         )
@@ -64,13 +82,13 @@ def update_book(
 
 @router.delete("/{book_id}")
 def delete_book(
-    book_id: int, 
-    db: Session = Depends(get_db), 
+    book_id: int,
+    db: Session = Depends(get_db),
     current_user: models.User = Depends(get_current_user)
 ):
     book = db.query(models.Book).filter(models.Book.id == book_id).first()
     if not book:
-            raise HTTPException(
+        raise HTTPException(
             status_code=404,
             detail="Book not found"
         )
