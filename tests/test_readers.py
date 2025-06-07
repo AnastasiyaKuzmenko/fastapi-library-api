1d272d6c48ad34227df0c06080f30e6828e9c793
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-06-07 21:23:44 +0300

    add test_update_reader_success()

--- tests/test_readers.py
+++ tests/test_readers.py
@@ -31,3 +31,44 @@ def test_create_reader_success(client, db):
     assert data["name"] == "Test Reader"
     assert data["email"] == "reader@example.com"
 
+
+def test_update_reader_success(client, db):
+    user = models.User(
+        email="test@example.com",
+        hashed_password=auth.get_password_hash("password123")
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    login_response = client.post("/users/login", data={
+        "username": "test@example.com",
+        "password": "password123"
+    })
+    assert login_response.status_code == 200
+    token = login_response.json()["access_token"]
+
+    headers = {"Authorization": f"Bearer {token}"}
+
+    reader = models.Reader(
+        name="Old Name",
+        email="old@example.com"
+    )
+
+    db.add(reader)
+    db.commit()
+    db.refresh(reader)
+
+    updated_reader = {
+        "name": "New Name",
+        "email": "new@example.com"
+    }
+
+    response = client.put(f"/readers/{reader.id}", json=updated_reader,  headers=headers)
+
+    assert response.status_code == 200
+    data = response.json()
+
+    assert data["name"] == updated_reader["name"]
+    assert data["email"] == updated_reader["email"]
+
