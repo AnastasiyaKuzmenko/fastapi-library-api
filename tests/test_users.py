eed4df199ff4e27252a9e2e74e38c02026e07ce0
Anastasia <anastasiya.e.kuzmenko@gmail.com>
2025-06-07 22:14:54 +0300

    add test_login_users_success()

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,4 +1,4 @@
-from app import models
+from app import models, auth
 
 
 def test_register_users_success(client, db):
@@ -13,3 +13,22 @@ def test_register_users_success(client, db):
 
     user_in_db = db.query(models.User).filter(models.User.email == user_data["email"]).first()
     assert user_in_db is not None
+
+
+def test_login_users_success(client, db):
+    user = models.User(
+        email="test@example.com",
+        hashed_password=auth.get_password_hash("password123")
+    )
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    login_response = client.post("/users/login", data={
+        "username": "test@example.com",
+        "password": "password123"
+    })
+    assert login_response.status_code == 200
+    data = login_response.json()
+    assert "access_token" in data
+    assert data["token_type"] == "bearer"
